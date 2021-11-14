ffc9797170e7e3963f7b514fe086798c83b06629
Aaditya <uaaditya759@gmail.com>
2021-11-14 11:23:59 +0530

    Update urls.js

--- src/api/urls.js
+++ src/api/urls.js
@@ -4,3 +4,4 @@ const TeacherLogin = baseUrl + '/TeacherLogin';
 const StudentDetails = baseUrl + '/StudentDetails';
 const TeacherDetails = baseUrl + '/TeacherDetails';
 const PastLeaves = baseUrl + '/PastLeaves';
+// TODO: FILL UP THIS DOCUMENT WITH ALL THE ROUTES
