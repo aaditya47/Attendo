be2f2bd3b6c88c805cf183c575be9323373be076
Aaditya <uaaditya759@gmail.com>
2021-11-14 11:23:29 +0530

    Created urls.js to store all API endpoint links

---
+++ src/api/urls.js
@@ -0,0 +1,6 @@
+const baseUrl = 'http://127.0.0.1:8080';
+const StudentLogin = baseUrl + '/StudentLogin';
+const TeacherLogin = baseUrl + '/TeacherLogin';
+const StudentDetails = baseUrl + '/StudentDetails';
+const TeacherDetails = baseUrl + '/TeacherDetails';
+const PastLeaves = baseUrl + '/PastLeaves';
